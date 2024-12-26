8180ccc43a1027a0d4e4d4f4fc4aca52bfdc67fb
EthanVT97 <gyi798107@gmail.com>
2024-12-26 08:22:26 +0630

    Improve CORS handling with fallback configuration

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+// Set default CORS origin if not set
+if (!getenv('CORS_ORIGIN')) {
+    putenv('CORS_ORIGIN=*');
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
 header('Access-Control-Allow-Origin: ' . $_ENV['CORS_ORIGIN']);
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
